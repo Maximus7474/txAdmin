a2f216512050d8a3477366e4c495f02fd0daec4f
Maximus7474 <the.maximus.7474@gmail.com>
2026-06-15 09:25:51 +0200

    fix(resource/shared): set debug convar as not true by default

--- resource/shared.lua
+++ resource/shared.lua
@@ -52,7 +52,7 @@ TX_MENU_ENABLED = GetConvarBool('txAdmin-menuEnabled')
 -- Setting global debug variable for all files
 -- On the client, this is updated by receiving a `txcl:setDebugMode` event.
 -- On the server, this is updated by running txaSetDebugMode on Live Console
-TX_DEBUG_MODE = GetConvarBool('txAdmin-debugMode', true)
+TX_DEBUG_MODE = GetConvarBool('txAdmin-debugMode')
 
 
 --- Internal helper to format txAdmin console messages
